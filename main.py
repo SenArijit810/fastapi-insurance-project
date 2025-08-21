f9c8d57e9c299090a8cdd3ee319c8c1007c9dd7f
SenArijit810 <arijitsen8101@gmail.com>
2025-08-21 19:13:23 +0530

    Fix Backend Main.py file error in the code

--- main.py
+++ main.py
@@ -82,4 +82,4 @@ def predict_premium(data:UserInput):
 
     prediction=model.predict(input_df)[0]
 
-    return JSONResponse(status_code=200,content={'prediction_category':prediction})
+    return JSONResponse(status_code=200,content={'response':prediction})
